60e97146a176dca05628c39f6bc51e19b5f04822
LalithaKurma <lalitha_kurma@yahoo.com>
2017-02-06 03:42:44 -0800

    bootstrap add dnsdict6

--- ECCbootstrap.sh
+++ ECCbootstrap.sh
@@ -2,8 +2,7 @@ __apt_get_install_noinput() {
     apt-get install -y -o DPkg::Options::=--force-confold $@; return $?
 }
 install_recon_ng(){
-   packages="alien
-python-pip
+   packages="python-pip
 git
 python-pip
 python-dnspython
@@ -11,8 +10,7 @@ python-mechanize
 python-slowaes
 python-xlsxwriter
 python-jsonrpclib
-python-lxml
-perl"
+python-lxml"
    CDDR=$(pwd)
    cd /tmp/ECCTools/reconng
    packages="$packages"
@@ -30,15 +28,35 @@ perl"
     cp -r /tmp/ECCTools/reconng /home/cast/
     cd $CDDR
 }
-#install_perl(){
-#	 __apt_get_install_noinput "perl" >> $HOME/ECC-install.log 2>&1
-#        ERROR=$?
-#        if [ $ERROR -ne 0 ]; then
-#            echo "Install Failure: perl (Error Code: $ERROR)"
-#        else
-#            echo "Installed Package: perl"
-#        fi
-#}
+install_dnsenum_dependencies(){
+packages="alien
+perl"
+   echo "Installing dnsenum dependency packages"
+   for PACKAGE in $packages; do
+        __apt_get_install_noinput $PACKAGE >> $HOME/ECC-install.log 2>&1
+        ERROR=$?
+        if [ $ERROR -ne 0 ]; then
+            echo "Install Failure: $PACKAGE (Error Code: $ERROR)"
+        else
+            echo "Installed Package: $PACKAGE"
+        fi
+    done
+}
+
+install_dnsdict6_dependencies(){
+packages="libpcap0.8-dev 
+libssl-dev"
+   echo "Installing dnsdict6 dependency packages"
+   for PACKAGE in $packages; do
+        __apt_get_install_noinput $PACKAGE >> $HOME/ECC-install.log 2>&1
+        ERROR=$?
+        if [ $ERROR -ne 0 ]; then
+            echo "Install Failure: $PACKAGE (Error Code: $ERROR)"
+        else
+            echo "Installed Package: $PACKAGE"
+        fi
+    done
+}
 install_perl_modules(){
 	perl -MCPAN -e "install Getopt::Long"
 	perl -MCPAN -e "install IO::File"	
@@ -53,6 +71,17 @@ install_perl_modules(){
 	perl -MCPAN -e "install String::Random"
 	perl -MCPAN -e "install WWW::Mechanize"
 }
+install_dnsdict6(){
+	CDDR=$(pwd)
+	mkdir /tmp/dnsdict6
+	cp thc-ipv6-1.8.tar.gz /tmp/dnsdict6/
+	cd /tmp/dnsdict6
+	tar xzvf thc-ipv6-1.8.tar.gz
+	cd thc-ipv6-1.8/
+	make
+	make install
+	cd $CDDR
+}
 install_ECC_Tools() {
   # Installing Burp suite from ECCTools Github Repository
   echo "ECC tools: Copying ECC Tools"
@@ -62,30 +91,28 @@ install_ECC_Tools() {
 	#bash burpsuite_free_linux_v1_7_16.sh >> $HOME/ECC-install.log 2>&1
         #gdebi netdiscover_0.3beta7~pre+svn118-1_amd64.deb
 	printf "\n"
-	echo "* Info: Installing NetDiscover Tool..."        
-	dpkg -i netdiscover_0.3beta7~pre+svn118-1_amd64.deb && apt install -f
-	echo "ECC tools: Completed NetDiscover Tool Installation"
-	printf "\n"
-	echo "* Info: Installing Nmap Tool..."        
-	dpkg -i nmap_7.40-2_amd64.deb && apt install -f
-        echo "ECC tools: Completed Nmap Tool Installation"
-	printf "\n"
-	echo "* Info: Installing Zenmap Tool..."        
-	dpkg -i zenmap_7.40-2_all.deb && apt install -f
-        echo "ECC tools: Completed Zenmap Tool Installation"
-	printf "\n"
-	echo "* Info: Installing recon-ng Tool..."
-	install_recon_ng
-	echo "ECC tools: Completed recon-ng Installation"
-	printf "\n"
+	#echo "* Info: Installing NetDiscover Tool..."        
+	#dpkg -i netdiscover_0.3beta7~pre+svn118-1_amd64.deb && apt install -f
+	#echo "ECC tools: Completed NetDiscover Tool Installation"
+	#printf "\n"
+	#echo "* Info: Installing Nmap Tool..."        
+	#dpkg -i nmap_7.40-2_amd64.deb && apt install -f
+        #echo "ECC tools: Completed Nmap Tool Installation"
+	#printf "\n"
+	#echo "* Info: Installing Zenmap Tool..."        
+	#dpkg -i zenmap_7.40-2_all.deb && apt install -f
+        #echo "ECC tools: Completed Zenmap Tool Installation"
+	#printf "\n"
+	#echo "* Info: Installing recon-ng Tool..."
+	#install_recon_ng
+	#echo "ECC tools: Completed recon-ng Installation"
+	#printf "\n"
 	#echo "* Info: Installing Snmpcheck Tool..."        
 	#dpkg -i snmpcheck_1.8-5_all.deb && apt install -f
         #echo "ECC tools: Completed Snmpcheck Installation"
 
-	#echo "ECC tools: Installing Perl..."
-	#install_perl
-	#echo "ECC tools: Installed Perl"
-
+		
+	install_dnsenum_dependencies
 	echo "ECC tools: Installing Perl Modules"
 	install_perl_modules
 	echo "ECC tools: Installed Perl Modules"
@@ -93,7 +120,11 @@ install_ECC_Tools() {
 	echo "* Info: Installing dnsenum Tool..."        
 	dpkg -i dnsenum_1.2.4.2-6_all.deb && apt install -f
         echo "ECC tools: Completed dnsenum Installation"
-
+	printf "\n"
+	echo "* Info: Installing dnsdict6 Tool..."
+	install_dnsdict6_dependencies
+	install_dnsdict6
+	echo "ECC tools: Completed dnsdict6 Installation"
         cd $CDIR
 	rm -r -f /tmp/ECCTools
 }
