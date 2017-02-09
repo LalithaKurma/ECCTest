62227fd45c08728b56d413aa5d2c0b39e68f3a42
LalithaKurma <lalitha_kurma@yahoo.com>
2017-02-08 21:58:42 -0800

    ECCbootstrap metasploit

--- ECCbootstrap.sh
+++ ECCbootstrap.sh
@@ -161,6 +161,7 @@ install_metasploit(){
 	CDDR=$(pwd)
 	cd /home/cast/
 	echo "Installing and Configuring RVM"
+
 	#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 	#\curl -sSL https://get.rvm.io | bash -s stable --ruby
 	curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
@@ -243,7 +244,7 @@ install_ECC_Tools() {
 	install_metasploit_dependencies
 	install_metasploit
 	echo "ECC tools: Completed Metasploit Framework Installation"
-	printf "\n"
+#	printf "\n"
 
  #       cd $CDIR
 #	rm -r -f /tmp/ECCTools
